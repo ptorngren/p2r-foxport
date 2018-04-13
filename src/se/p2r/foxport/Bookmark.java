296d66b58a9db9eba71f0fc2d0569ea6d7e27140
Peer Törngren <reg-github@p2r.se>
2018-04-13 13:03:59 +0200

    javadoc

--- src/se/p2r/foxport/Bookmark.java
+++ src/se/p2r/foxport/Bookmark.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 /**
  * An abstraction of a bookmark, providing the essential attributes of a
- * bookmark.
+ * bookmark. The bookmark may be a folder ({@link #isContainer()}) which may or
+ * may not have children (links or containers), or a link ({@link #isLink()})
+ * that carries a URI. Both links and containers may carry a human readable
+ * description. All bookmarks are expected to carry a name.
  * 
  * @author peer
  *
@@ -14,7 +17,7 @@ public interface Bookmark {
 	List<? extends Bookmark> getChildren();
 
 	/**
-	 * The name of the bookmark, as it appears in the browser (and in the genertated
+	 * The name of the bookmark, as it appears in the browser (and in the generated
 	 * file).
 	 * 
 	 * @return String (not <code>null</code>)
@@ -23,12 +26,14 @@ public interface Bookmark {
 
 	/**
 	 * Must not return the same result as {@link #isContainer()}.
+	 * 
 	 * @return boolean
 	 */
 	boolean isLink();
 
 	/**
 	 * Must not return the same result as {@link #isLink()}.
+	 * 
 	 * @return boolean
 	 */
 	boolean isContainer();
@@ -51,6 +56,7 @@ public interface Bookmark {
 
 	/**
 	 * Return a string describing the bookmark, if it exists.
+	 * 
 	 * @return String or <code>null</code>
 	 */
 	String getDescription();
